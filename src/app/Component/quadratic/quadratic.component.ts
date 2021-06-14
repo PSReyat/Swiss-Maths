adffc2883d563e8525d1d3b3d73238ce693ba295
Parminder <preyat94@gmail.com>
2021-06-14 17:11:48 +0100

    added more solving functionality

--- src/app/Component/quadratic/quadratic.component.ts
+++ src/app/Component/quadratic/quadratic.component.ts
@@ -9,6 +9,10 @@ import { Component, OnInit, Input } from '@angular/core';
 export class QuadraticComponent implements OnInit {
 
   disc: number | string;
+  solution1: number;
+  solution2: number;
+  factor1: number;
+  factor2: number;
   a: number;
   b: number;
   c: number;
@@ -24,8 +28,18 @@ export class QuadraticComponent implements OnInit {
     this.disc = Math.pow(b, 2) - (4 * a * c);
 
     if(this.disc < 0){
-      this.disc = "error"
+      this.disc = "error: no real roots.";
+      return this.disc;
     }
+
+    this.solution1 = (b + Math.sqrt(this.disc))/(2 * a);
+    console.log((b + Math.sqrt(this.disc)));
+    console.log(Math.sqrt(this.disc) + " b: " + b + " a: " + a);
+    this.solution2 = (b - Math.sqrt(this.disc))/(2 * a);
+    console.log(this.solution2);
+
+    this.factor1 = (-1) * this.solution1;
+    this.factor2 = (-1) * this.solution2;
 
   }
 
